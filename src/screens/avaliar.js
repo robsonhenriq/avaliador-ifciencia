555df6d0e0bacf4c1bf35762f25a087b87b5520d
Robson Henrique <robsonhenrique16@hotmail.com>
2019-09-15 20:03:18 -0300

    [update] Mudando a propriedade Titulo, para titulo, pois o JSON lido no poster está assim, e por causa disso dava: Titulo undefined

--- src/screens/avaliar.js
+++ src/screens/avaliar.js
@@ -1,66 +1,69 @@
-
-import React, { Component } from 'react';
-import colors from '../styles/colors';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import colors from "../styles/colors";
+import { connect } from "react-redux";
 
 import {
   Alert,
   StyleSheet,
   Text,
   TouchableOpacity,
-  Linking,
-} from 'react-native';
- 
-import QRCodeScanner from 'react-native-qrcode-scanner';
- 
+  Linking
+} from "react-native";
+
+import QRCodeScanner from "react-native-qrcode-scanner";
+
 class ScreenAvaliar extends Component {
-  
   static navigationOptions = {
     title: "Scanear Poster",
     headerStyle: {
-        backgroundColor: colors.primary,
-      },
-      headerTintColor: colors.titleFontColor,
-      headerTitleStyle: {
-        fontWeight: 'bold',
-        textAlign: 'center',
-      },
-  }
+      backgroundColor: colors.primary
+    },
+    headerTintColor: colors.titleFontColor,
+    headerTitleStyle: {
+      fontWeight: "bold",
+      textAlign: "center"
+    }
+  };
   onSuccess(e) {
     const data = JSON.parse(e.data);
     const { navigation } = this.props;
     const { avaliacoesList } = this.props.avaliacoes;
 
-    let avaliacaoJaRealizada = avaliacoesList.find((avaliacao) => {
-        return avaliacao.posterId === data.Id
+    let avaliacaoJaRealizada = avaliacoesList.find(avaliacao => {
+      return avaliacao.posterId === data.Id;
     });
 
-    if (avaliacaoJaRealizada){
-
+    if (avaliacaoJaRealizada) {
       Alert.alert("Este Poster já foi avaliado", "", [
-        {text: 'Ok. Irei scanear outro.', onPress: () => this.scanner.reactivate()}]
-      );
-
-    }
-    else {
-
-      Alert.alert("Deseja avaliar este Poster?", `Titulo: ${data.Titulo}`, [
-        {text: 'Cancelar', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-        {text: 'Confirmar', onPress: () => navigation.navigate("Questionario", { data: data })}
+        {
+          text: "Ok. Irei scanear outro.",
+          onPress: () => this.scanner.reactivate()
+        }
       ]);
-
+    } else {
+      Alert.alert("Deseja avaliar este Poster?", `Titulo: ${data.titulo}`, [
+        {
+          text: "Cancelar",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        {
+          text: "Confirmar",
+          onPress: () => navigation.navigate("Questionario", { data: data })
+        }
+      ]);
     }
   }
- 
+
   render() {
     return (
       <QRCodeScanner
-        ref={(node) => { this.scanner = node }}
-        onRead={(e) => this.onSuccess(e)}
+        ref={node => {
+          this.scanner = node;
+        }}
+        onRead={e => this.onSuccess(e)}
         topContent={
-          <Text style={styles.centerText}>
-            Scaneie o poster e avalie
-          </Text>
+          <Text style={styles.centerText}>Scaneie o poster e avalie</Text>
         }
         bottomContent={
           <TouchableOpacity style={styles.buttonTouchable}>
@@ -71,30 +74,30 @@ class ScreenAvaliar extends Component {
     );
   }
 }
- 
+
 const styles = StyleSheet.create({
   centerText: {
     flex: 1,
     fontSize: 18,
     padding: 32,
-    color: '#777',
+    color: "#777"
   },
   textBold: {
-    fontWeight: '500',
-    color: '#000',
+    fontWeight: "500",
+    color: "#000"
   },
   buttonText: {
     fontSize: 21,
-    color: 'rgb(0,122,255)',
+    color: "rgb(0,122,255)"
   },
   buttonTouchable: {
-    padding: 16,
-  },
+    padding: 16
+  }
 });
 
-const mapStateToProps = (state) => {
-  const { avaliacoes } = state
-  return { avaliacoes }
+const mapStateToProps = state => {
+  const { avaliacoes } = state;
+  return { avaliacoes };
 };
 
 export default connect(mapStateToProps)(ScreenAvaliar);
